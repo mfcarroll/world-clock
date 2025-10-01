e6be6c11adea70acb8499e83db6f0acebfa71bdb
Matthew Carroll <matthew.carroll@stand.earth>
2025-10-01 03:59:39 -0400

    module config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // Vite will automatically detect and use the postcss.config.js 
-  // and tailwind.config.js files in your project root.
+  // Vite is designed to automatically detect postcss.config.js and tailwind.config.js
+  // in the project root. No extra css configuration is needed here.
 });
 
