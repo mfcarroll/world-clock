d2f84bdbd1fda7210613963b52dc5fd5ea5ff1d0
Matthew Carroll <matthew.carroll@stand.earth>
2025-10-01 03:52:14 -0400

    Postcss config

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,8 @@
 import { defineConfig } from 'vite';
-import postcss from 'postcss';
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // Base is set for GitHub Pages deployment. 
-  base: '/', 
-  build: {
-    outDir: 'dist',
-  },
-  // Explicitly define PostCSS plugins
-  css: {
-    postcss: {
-      plugins: [
-        tailwindcss,
-        autoprefixer,
-      ],
-    },
-  },
+  // Vite will automatically detect and use the postcss.config.js 
+  // and tailwind.config.js files in your project root.
 });
 
