3924fe1f1dcb3c1847fbef5862a9ff6c307cccc1
Matthew Carroll <matthew.carroll@stand.earth>
2025-10-04 19:34:05 -0700

    Build app prior to ios packaging

--- ios/App/ci_scripts/ci_post_clone.sh
+++ ios/App/ci_scripts/ci_post_clone.sh
@@ -2,26 +2,27 @@
 
 echo "--- SETTING UP ENVIRONMENT ---"
 
-# Install Homebrew if it's not already installed
-if ! command -v brew &> /dev/null
-then
-    echo "Homebrew not found. Installing..."
+# Install Homebrew if needed and add to PATH
+if ! command -v brew &> /dev/null; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
-
-# Add Homebrew to the PATH
 export PATH="/opt/homebrew/bin:$PATH"
 
 # Install Node.js
 echo "Installing Node.js..."
 brew install node
 
-# This script runs from the ios/App/ci_scripts directory.
 # Navigate to the root of the repository.
 cd ../../../
 
 echo "--- INSTALLING NPM DEPENDENCIES ---"
 npm install
+
+echo "--- BUILDING WEB APP ---"
+npm run build
+
+echo "--- SYNCING CAPACITOR PROJECT ---"
+npx cap sync ios
 
 # Navigate back to the iOS project directory to install pods.
 cd ios/App
