3667dd8639395ea8d8f48e0fead48c496e45e5c7
Matthew Carroll <matthew.carroll@stand.earth>
2025-10-05 23:44:12 -0700

    android config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,15 @@
+// capacitor.config.ts
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'ca.matthewcarroll.geotime',
   appName: 'GeoTime',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    hostname: 'geotime.local',
+    androidScheme: 'https',
+    iosScheme: 'https',
+  },
 };
 
 export default config;
